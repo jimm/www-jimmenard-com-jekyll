0e78c264b7df719b322b414a76b0779fdc22d32a
Jim Menard <jim@jimmenard.com>
2026-07-07 13:57:19 -0400

    update

--- bin/copy_resume_pdf.sh
+++ bin/copy_resume_pdf.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
+#
+# Copies resume into _site. Finds the first one in RESUME_LOCS.
 
 HERE="$(cd "$(dirname "$0")" >/dev/null && pwd)"
 DEST="${HERE}/../_site/Jim_Menard_resume.pdf"
-RESUME="${writing}/resume/Jim_Menard_resume.pdf"
-DL_RESUME="${dl}/Jim_Menard_resume.pdf"
-DL_RESUME_SPACES="${dl}/Jim Menard Resume.pdf"
+RESUME_LOCS=(
+    "${writing}/resume/Jim_Menard_resume.pdf"
+    "${dl}/Jim_Menard_resume.pdf"
+    "${dl}/Jim Menard Resume.pdf"
+)
 
 mkdir -p "$(dirname "$DEST")"
-if [ -f "$RESUME" ] ; then
-    cp "$RESUME" "$DEST"
-elif [ -f "$DL_RESUME" ] ; then
-    cp "$DL_RESUME" "$DEST"
-elif [ -f "$DL_RESUME_SPACES" ] ; then
-    cp "$DL_RESUME_SPACES" "$DEST"
-fi
-chmod 644 "$DEST"
+for path in "${RESUME_LOCS[@]}" ; do
+    if [ -f "$path" ] ; then
+        cp "$path" "$DEST"
+        chmod 644 "$DEST"
+        exit 0
+    fi
+done
+echo no resume found to copy
